f65365c04f2b1e20cdaa9a33435bd2bac9eb7a29
juanpablo.espinola <juanpablo.espinola@gmail.com>
2021-03-24 21:48:16 -0300

    Entrega Clase 01 JS

---
+++ js/archivoespinola.js
@@ -0,0 +1,12 @@
+let myName = prompt("Ingrese su nombre completo por favor");
+alert("Bienvenido al Mundo de ESPINOLA " + myName);
+console.log(myName);
+alert("Para una atención personalizada y en la búsqueda de un mejor calce en la ropa, a continuación le haremos las siguientes preguntas:");
+let q1 = parseInt(prompt("Ingrese su contorno de busto:"));
+console.log(q1);
+let q2 = parseInt(prompt("Ingrese su contorno de cintura:"));
+console.log(q2);
+let q3 = parseInt(prompt("Ingrese su contorno de cadera:"));
+console.log(q3);
+alert("La suma de tus medidas es " + (q1 + q2 + q3));
+alert("A continuación, se te abrirá la página oficial de ESPINOLA");
